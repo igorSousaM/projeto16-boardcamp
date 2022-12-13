54e09a41c78243b3cff2b8edb1b5a5fe8e95853c
Igor-S-M <igorsousa3148@gmail.com>
2022-12-13 19:43:45 -0300

    fix: add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 const port = process.env.PORT;
